12e0b0b819a6fe5f8ab3ad23485fd443027acd19
tylerscave <tylerjones365@gmail.com>
2017-05-15 00:27:53 -0700

    kind of working delete done, but still buggy

--- RouteTracker/SavedRouteListViewController.swift
+++ RouteTracker/SavedRouteListViewController.swift
@@ -12,20 +12,21 @@
 
 import UIKit
 import Foundation
+import CoreData
 
 class SavedRouteListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     let dateFormatter = DateFormatter()
     var deleteRouteIndexPath: NSIndexPath? = nil
-    
+    var myRoutes = MyRoutes.sharedInstance.allRoutes
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         tableView.dataSource = self
         tableView.delegate = self
         dateFormatter.dateStyle = .long
         dateFormatter.timeStyle = .short
+        myRoutes = MyRoutes.sharedInstance.allRoutes
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,83 +34,40 @@ class SavedRouteListViewController: UIViewController, UITableViewDataSource, UIT
         self.tableView.reloadData()
     }
 
-    //return a cell row
+    // create cells containing the routes and populate the table
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell")!
-
-        let allRoutes = MyRoutes.sharedInstance.allRoutes
-        let selectedRoute = allRoutes[indexPath.row]
+        let selectedRoute = myRoutes[indexPath.row]
         cell.textLabel?.text = dateFormatter.string(from: selectedRoute.startTimeStamp as Date)
             
         return cell
     }
     
-    //index of each cell
+    // number of routes in table
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return MyRoutes.sharedInstance.allRoutes.count
+        return myRoutes.count
     }
     
-    // method to run when table view cell is tapped
+    // runs when a cell is selected
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let allRoutes = MyRoutes.sharedInstance.allRoutes
-        let route = allRoutes[indexPath.row] as MyRoute
+        let route = myRoutes[indexPath.row] as MyRoute
         MyRoutes.sharedInstance.selectedtRoute = route
         self.dismiss(animated: true, completion: nil)
     }
-    
-    // this method handles row deletion
-    //used from http://stackoverflow.com/questions/3309484/uitableviewcell-show-delete-button-on-swipe/37719543#37719543
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-//        let allRoutes = MyRoutes.sharedInstance.allRoutes
-        if editingStyle == .delete {
-            
-            // remove the item from the data model
-            deleteRouteIndexPath = indexPath as NSIndexPath
-//            let routeToDelete = allRoutes[indexPath.row]
-//            confirmDelete(routeToRemove: routeToDelete)
-            confirmDelete()
-//            animals.remove(at: indexPath.row)
-            
-            // delete the table view row
-//            tableView.deleteRows(at: [indexPath], with: .fade)
-            
-//        } else if editingStyle == .insert {
-            // Not used in our example, but if you were adding a new row, this is where you would do it.
-        }
-    }
-    
-//    func confirmDelete(routeToRemove :MyRoute){
-    func confirmDelete(){
-        let alert = UIAlertController(title: "Delete Route", message: "Do you want to delete this route?", preferredStyle: .actionSheet)
-        
-//        let saveAction = UIAlertAction(title: "Save", style: .destructive) { (alert: UIAlertAction!) -> Void in
-            let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: handleDeleteRoute)
-            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: cancelDeleteRoute)
-        
-            alert.addAction(deleteAction)
-            alert.addAction(cancelAction)
-        
-            self.present(alert, animated: true, completion:nil)
-    }
-    
-    func handleDeleteRoute(alertAction: UIAlertAction!) -> Void {
-        var allRoutes = MyRoutes.sharedInstance.allRoutes
-        if let indexPath = deleteRouteIndexPath {
-            tableView.beginUpdates()
-            
-            allRoutes.remove(at: indexPath.row)
-            
-            // Note that indexPath is wrapped in an array:  [indexPath]
-            tableView.deleteRows(at: [indexPath as IndexPath], with: .automatic)
-            
-            deleteRouteIndexPath = nil
-            
-            tableView.endUpdates()
-        }
-    }
-    
-    func cancelDeleteRoute(alertAction: UIAlertAction!) {
-        deleteRouteIndexPath = nil
-    }
 
+    // delete a route swiping left
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        let myRoute = myRoutes[indexPath.row]
+        if editingStyle == .delete {
+            context.delete(myRoute)
+            myRoutes.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
+            do {
+                try context.save()
+            } catch let error as NSError {
+                print("Error While Deleting Route: \(error.userInfo)")
+            }
+        }
+        self.tableView.reloadData()
+    }
 }
